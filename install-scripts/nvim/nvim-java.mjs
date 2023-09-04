1d4bf543f75b9ea15d295121eaaae1cb7394bba9
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-09-04 06:25:34 +0000

    update

--- install-scripts/nvim/nvim-java.mjs
+++ install-scripts/nvim/nvim-java.mjs
@@ -3,8 +3,8 @@
 const homeDir = os.homedir()
 const installFolder = `${homeDir}/.local/share/nvim`
 
-// await $`git clone https://github.com/microsoft/java-debug.git ${installFolder}/java-debug`.nothrow()
-// await $`cd ${installFolder}/java-debug && ./mvnw clean install`
+await $`git clone https://github.com/microsoft/java-debug.git ${installFolder}/java-debug`.nothrow()
+await $`cd ${installFolder}/java-debug && ./mvnw clean install`
 
 await $`git clone https://github.com/microsoft/vscode-java-test.git ${installFolder}/vscode-java-test`.nothrow()
 await $`cd ${installFolder}/vscode-java-test && npm install && npm run build-plugin`
