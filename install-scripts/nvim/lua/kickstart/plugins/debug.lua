386b2005d41b7fe8d60e715aafe621ca0fc173b1
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-09-04 16:28:29 +0000

    finally working

--- install-scripts/nvim/lua/kickstart/plugins/debug.lua
+++ install-scripts/nvim/lua/kickstart/plugins/debug.lua
@@ -83,24 +83,27 @@ return {
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
     require('dap-python').setup(vim.env.HOME .. '/.virtualenvs/debugpy/bin/python')
-    local bundles = {
-      vim.fn.glob(
-        vim.fn.stdpath('data') ..
-        "/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
-      ),
-    }
-    vim.list_extend(bundles, vim.split(vim.fn.glob(vim.fn.stdpath('data') .. "/vscode-java-test/server/*.jar"), "\n"))
-    require('jdtls').start_or_attach({
-      cmd = { vim.fn.stdpath('data') .. '/mason/bin/jdtls' },
-      root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
-      init_options = {
-        bundles = bundles
-      },
-    })
-    require('jdtls').setup_dap({ hotcodereplace = 'auto' })
-    vim.schedule(vim.schedule_wrap(function()
-      vim.wait(3000)
-      require('jdtls.dap').setup_dap_main_class_configs()
+
+    async_load_dap = vim.loop.new_async(vim.schedule_wrap(function()
+      local bundles = {
+        vim.fn.glob(
+          vim.fn.stdpath('data') ..
+          "/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
+        ),
+      }
+      vim.list_extend(bundles, vim.split(vim.fn.glob(vim.fn.stdpath('data') .. "/vscode-java-test/server/*.jar"), "\n"))
+
+      require('jdtls').start_or_attach({
+        cmd = { vim.fn.stdpath('data') .. '/mason/bin/jdtls' },
+        root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+        init_options = {
+          bundles = bundles
+        },
+      })
+      require('jdtls').setup_dap({ hotcodereplace = 'auto' })
+      vim.defer_fn(function() require('jdtls.dap').setup_dap_main_class_configs() end, 5000)
+      async_load_dap:close()
     end))
+    async_load_dap:send()
   end,
 }
