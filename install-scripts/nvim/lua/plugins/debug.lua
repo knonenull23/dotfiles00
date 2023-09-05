4b83147e3bec005f79ae1edf3f42dbc96336d23a
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-09-05 17:15:57 +0000

    manually activate debugger

--- install-scripts/nvim/lua/plugins/debug.lua
+++ install-scripts/nvim/lua/plugins/debug.lua
@@ -25,6 +25,30 @@ return {
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
     end, { desc = 'Debug: Set Breakpoint' })
     vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+    vim.keymap.set('n', '<F12>', function()
+      require('dap-python').setup(vim.env.HOME .. '/.virtualenvs/debugpy/bin/python')
+      async_load_dap = vim.loop.new_async(vim.schedule_wrap(function()
+        local bundles = {
+          vim.fn.glob(
+            vim.fn.stdpath('data') ..
+            "/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
+          ),
+        }
+        vim.list_extend(bundles, vim.split(vim.fn.glob(vim.fn.stdpath('data') .. "/vscode-java-test/server/*.jar"), "\n"))
+
+        require('jdtls').start_or_attach({
+          cmd = { vim.fn.stdpath('data') .. '/mason/bin/jdtls' },
+          root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+          init_options = {
+            bundles = bundles
+          },
+        })
+        require('jdtls').setup_dap({ hotcodereplace = 'auto' })
+        vim.defer_fn(function() require('jdtls.dap').setup_dap_main_class_configs() end, 8000)
+        async_load_dap:close()
+      end))
+      async_load_dap:send()
+    end, { desc = 'Activate Debuggers' })
 
     dapui.setup {
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
@@ -46,29 +70,5 @@ return {
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-    -- require('dap-python').setup(vim.env.HOME .. '/.virtualenvs/debugpy/bin/python')
-    --
-    -- async_load_dap = vim.loop.new_async(vim.schedule_wrap(function()
-    --   local bundles = {
-    --     vim.fn.glob(
-    --       vim.fn.stdpath('data') ..
-    --       "/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
-    --     ),
-    --   }
-    --   vim.list_extend(bundles, vim.split(vim.fn.glob(vim.fn.stdpath('data') .. "/vscode-java-test/server/*.jar"), "\n"))
-    --
-    --   require('jdtls').start_or_attach({
-    --     cmd = { vim.fn.stdpath('data') .. '/mason/bin/jdtls' },
-    --     root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
-    --     init_options = {
-    --       bundles = bundles
-    --     },
-    --   })
-    --   require('jdtls').setup_dap({ hotcodereplace = 'auto' })
-    --   vim.defer_fn(function() require('jdtls.dap').setup_dap_main_class_configs() end, 8000)
-    --   async_load_dap:close()
-    -- end))
-    -- async_load_dap:send()
   end,
 }
