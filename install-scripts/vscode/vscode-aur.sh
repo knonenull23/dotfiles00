bf191ce31f13238e6178d6a29ccfc023b5c6385b
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-09-08 22:31:13 +0800

    Update vscode-aur.sh

--- install-scripts/vscode/vscode-aur.sh
+++ install-scripts/vscode/vscode-aur.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-git clone https://aur.archlinux.org/visual-studio-code-bin.git $HOME/visual-studeio-code-bin
+git clone https://aur.archlinux.org/visual-studio-code-bin.git $HOME/visual-studio-code-bin
 cd $HOME/visual-studio-code-bin 
 makepkg -si
 cd $HOME; rm -rf $HOME/visual-studio-code-bin
