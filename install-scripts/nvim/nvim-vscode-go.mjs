84f88d7cf78f7e41238a4b1f26f7d13b7fcd97c0
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-09-08 12:51:31 +0000

    update

--- install-scripts/nvim/nvim-vscode-go.mjs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zx
-
-const homeDir = os.homedir()
-const installFolder = `${homeDir}/.local/share/nvim`
-
-await $`git clone https://github.com/golang/vscode-go ${installFolder}/vscode-go`.nothrow()
-await $`cd ${installFolder}/vscode-go && npm install && npm run compile`
-
