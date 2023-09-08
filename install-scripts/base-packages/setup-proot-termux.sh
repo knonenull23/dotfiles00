7e7b3f840b9927c12dc7f6b33f1e31dd4351192b
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-09-08 09:19:03 +0000

    update

--- install-scripts/base-packages/setup-proot-termux.sh
+++ install-scripts/base-packages/setup-proot-termux.sh
@@ -5,3 +5,5 @@ pkg update
 pkg install x11-repo
 pkg install proot proot-distro termux-x11-nightly xdotool golang delve
 proot-distro install archlinux
+
+ln -s $PREFIX/var/lib/proot-distro/installed-rootfs/archlinux/home/arch/ $HOME/arch
--- install-scripts/leetcode-cli/leetcode-cli.mjs
+++ install-scripts/leetcode-cli/leetcode-cli.mjs
@@ -1,4 +1,3 @@
 #!/usr/bin/env zx
 
-await $`sudo apt install build-essential libssl-dev libdbus-1-dev libsqlite3-dev -y`
 await $`cargo install leetcode-cli`
