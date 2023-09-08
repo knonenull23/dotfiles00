9818bc70720022ee71804bdd2fa7413d8eac93ab
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-09-08 16:31:57 +0800

    Update base-pkgs.sh

--- install-scripts/base-packages/base-pkgs.sh
+++ install-scripts/base-packages/base-pkgs.sh
@@ -1,7 +1,2 @@
-# sudo pacman -Syu
-# sudo pacman -S base-devel git tmux neovim xfce4 github-cli zip ripgrep dnsutils python python-pip
-
-mkdir $HOME/.virtualenvs
-cd $HOME/.virtualenvs
-python -m venv debugpy
-debugpy/bin/python -m pip install debugpy
+sudo pacman -Syu
+sudo pacman -S base-devel git tmux neovim xfce4 github-cli zip ripgrep dnsutils python python-pip
