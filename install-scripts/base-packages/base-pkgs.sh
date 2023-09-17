d82d4e12c764c73d0a1ab50b058d2d6c6611881e
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-09-17 04:44:15 +0000

    update

--- install-scripts/base-packages/base-pkgs.sh
+++ install-scripts/base-packages/base-pkgs.sh
@@ -1,2 +1,2 @@
 sudo pacman -Syu
-sudo pacman -S base-devel git tmux neovim xfce4 github-cli zip ripgrep dnsutils python python-pip
+sudo pacman -S base-devel git git-delta tmux neovim xfce4 github-cli zip ripgrep dnsutils python python-pip
