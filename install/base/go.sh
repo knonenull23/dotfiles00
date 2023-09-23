11db00e9d4db69153afc9cdc8390bb00413454d7
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-09-23 15:28:09 +0800

    Update go.sh

--- install/base/go.sh
+++ install/base/go.sh
@@ -5,7 +5,7 @@ architecture=""
 case $(uname -m) in
     x86_64) architecture="amd64" ;;
     arm64)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
-    aarch64)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
+    aarch64)  dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
 esac
 
 echo $architecture
