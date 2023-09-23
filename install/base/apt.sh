be52e2b084e142f4a828352ec515ed9e6cc1c91a
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-09-23 21:58:41 +0800

    Update apt.sh

--- install/base/apt.sh
+++ install/base/apt.sh
@@ -4,4 +4,7 @@ set -e
 apt install zip curl git wget jq python3-pip python3-venv tmux build-essential -y
 
 mkdir -p "$HOME/.local"
-echo 'export PATH=$PATH:$HOME/.local/bin' >> "$HOME/.bashrc"
+
+printf '⚠️  ADD (OR REPLACE) THIS LINE BELOW TO YOUR ~/.bashrc  ⚠️
+export PATH=$PATH:$HOME/.local/bin
+\n'
