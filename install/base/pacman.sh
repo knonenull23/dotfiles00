075ddcfef14da1c3ec92d5d5aa2239b904c5e387
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-09-23 21:32:08 +0800

    Update pacman.sh

--- install/base/pacman.sh
+++ install/base/pacman.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-pacman -Sy install zip curl git wget jq python python-pip tmux base-devel 
+pacman -Sy zip curl git wget jq python python-pip tmux base-devel 
 
 mkdir -p "$HOME/.local"
 echo 'export PATH=$PATH:$HOME/.local/bin' >> "$HOME/.bashrc"
