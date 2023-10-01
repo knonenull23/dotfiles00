905cf70b659482704f57a7ff9472fb6dac72cbb9
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-10-01 14:44:13 +0800

    update

--- conf/nvim/init.lua
+++ conf/nvim/init.lua
@@ -44,3 +44,50 @@ vim.o.timeoutlen = 200
 vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
 vim.opt.path = vim.opt.path + { vim.fn.stdpath('data') .. 'mason/bin' }
+vim.o.foldmethod = 'indent'
+vim.o.foldlevelstart = 99
+vim.o.foldnestmax=2;
+
+function ToggleSideBar()
+    if vim.wo.number == true then
+        vim.wo.number = false
+        vim.opt.scl = 'no'
+    else
+        vim.wo.number = true
+        vim.opt.scl = 'yes'
+    end
+end
+
+vim.keymap.set("n", '<A-z>', 'za', { desc = "Fold" })
+vim.keymap.set("n", '<A-Z>', 'za', { desc = "Fold" })
+vim.keymap.set('n', '<A-n>', '<Esc><CMD>tabnew<CR>', {})
+vim.keymap.set('n', '<A-t>', '<Esc><CMD>terminal<CR>', {})
+vim.keymap.set('n', '<A-w>', '<Esc><CMD>q!<CR>', {})
+vim.keymap.set('n', '<A-x>', '<Esc><C-w>T<CR>', {})
+vim.keymap.set('n', '<A-X>', '<Esc><C-w><C-o>', {})
+vim.keymap.set('t', '<C-d>', '<C-\\><C-n>', {})
+vim.keymap.set('n', '<A-j>', '<C-W>j', {})
+vim.keymap.set('n', '<A-k>', '<C-W>k', {})
+vim.keymap.set('n', '<A-h>', '<C-W>h', {})
+vim.keymap.set('n', '<A-l>', '<C-W>l', {})
+vim.keymap.set('n', '<A-1>', '1gt', {})
+vim.keymap.set('n', '<A-2>', '2gt', {})
+vim.keymap.set('n', '<A-3>', '3gt', {})
+vim.keymap.set('n', '<A-4>', '4gt', {})
+vim.keymap.set('n', '<A-5>', '5gt', {})
+vim.keymap.set('n', '<A-6>', '6gt', {})
+vim.keymap.set('n', '<A-7>', '7gt', {})
+vim.keymap.set('n', '<A-8>', '8gt', {})
+vim.keymap.set('n', '<A-9>', '9gt', {})
+vim.keymap.set('n', '<A-<>', '<CMD>tabm -1<CR>', {})
+vim.keymap.set('n', '<A->>', '<CMD>tabm +1<CR>', {})
+vim.keymap.set('t', '<A-1>', '<C-\\><C-n>1gt', {})
+vim.keymap.set('t', '<A-2>', '<C-\\><C-n>2gt', {})
+vim.keymap.set('t', '<A-3>', '<C-\\><C-n>3gt', {})
+vim.keymap.set('t', '<A-4>', '<C-\\><C-n>4gt', {})
+vim.keymap.set('t', '<A-5>', '<C-\\><C-n>5gt', {})
+vim.keymap.set('t', '<A-6>', '<C-\\><C-n>6gt', {})
+vim.keymap.set('t', '<A-7>', '<C-\\><C-n>7gt', {})
+vim.keymap.set('t', '<A-8>', '<C-\\><C-n>8gt', {})
+vim.keymap.set('t', '<A-9>', '<C-\\><C-n>9gt', {})
+vim.keymap.set('i', '<C-f>', '<C-x><C-f>', {})
--- conf/nvim/lua/plugins/nvim-tree.lua
+++ conf/nvim/lua/plugins/nvim-tree.lua
@@ -3,18 +3,11 @@ return {
     {
         'nvim-tree/nvim-tree.lua',
         config = function()
-            function ToggleSideBar()
-                if vim.wo.number == true then
-                    vim.wo.number = false
-                    vim.opt.scl = 'no'
-                else
-                    vim.wo.number = true
-                    vim.opt.scl = 'yes'
-                end
-            end
-
-            vim.opt.termguicolors = true
             require('nvim-tree').setup({
+                filters = {
+                    dotfiles = false,
+                    custom = { '^.git$' }
+                },
                 disable_netrw = true,
                 git = {
                     ignore = true
@@ -37,41 +30,9 @@ return {
                     }
                 }
             })
-            vim.keymap.set('n', '<A-n>', '<Esc><CMD>tabnew<CR>', {})
-            vim.keymap.set('n', '<A-t>', '<Esc><CMD>terminal<CR>', {})
-            vim.keymap.set('n', '<A-w>', '<Esc><CMD>q!<CR>', {})
-            vim.keymap.set('n', '<A-x>', '<Esc><C-w>T<CR>', {})
-            vim.keymap.set('n', '<A-X>', '<Esc><C-w><C-o>', {})
-            vim.keymap.set('t', '<C-d>', '<C-\\><C-n>', {})
-            vim.keymap.set('n', '<A-j>', '<C-W>j', {})
-            vim.keymap.set('n', '<A-k>', '<C-W>k', {})
-            vim.keymap.set('n', '<A-h>', '<C-W>h', {})
-            vim.keymap.set('n', '<A-l>', '<C-W>l', {})
             vim.keymap.set('n', '<A-f>', '<CMD>Format<CR>', {})
-            vim.keymap.set('n', '<A-z>', '<CMD>lua ToggleSideBar()<CR>', {})
-            vim.keymap.set('n', '<A-1>', '1gt', {})
-            vim.keymap.set('n', '<A-2>', '2gt', {})
-            vim.keymap.set('n', '<A-3>', '3gt', {})
-            vim.keymap.set('n', '<A-4>', '4gt', {})
-            vim.keymap.set('n', '<A-5>', '5gt', {})
-            vim.keymap.set('n', '<A-6>', '6gt', {})
-            vim.keymap.set('n', '<A-7>', '7gt', {})
-            vim.keymap.set('n', '<A-8>', '8gt', {})
-            vim.keymap.set('n', '<A-9>', '9gt', {})
-            vim.keymap.set('n', '<A-<>', '<CMD>tabm -1<CR>', {})
-            vim.keymap.set('n', '<A->>', '<CMD>tabm +1<CR>', {})
-            vim.keymap.set('t', '<A-1>', '<C-\\><C-n>1gt', {})
-            vim.keymap.set('t', '<A-2>', '<C-\\><C-n>2gt', {})
-            vim.keymap.set('t', '<A-3>', '<C-\\><C-n>3gt', {})
-            vim.keymap.set('t', '<A-4>', '<C-\\><C-n>4gt', {})
-            vim.keymap.set('t', '<A-5>', '<C-\\><C-n>5gt', {})
-            vim.keymap.set('t', '<A-6>', '<C-\\><C-n>6gt', {})
-            vim.keymap.set('t', '<A-7>', '<C-\\><C-n>7gt', {})
-            vim.keymap.set('t', '<A-8>', '<C-\\><C-n>8gt', {})
-            vim.keymap.set('t', '<A-9>', '<C-\\><C-n>9gt', {})
             vim.keymap.set('n', '<A-q>', '<CMD>NvimTreeToggle<CR>', {})
             vim.keymap.set('n', '<A-S>', '<CMD>NvimTreeFindFile<CR>', {})
-            vim.keymap.set('i', '<C-f>', '<C-x><C-f>', {})
         end
     },
 
