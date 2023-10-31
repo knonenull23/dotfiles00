23ab5adc5ed22846fcd286b66146d66155fc7a03
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-10-31 10:26:18 +0800

    update

--- conf/nvim/lua/plugins/telescope.lua
+++ conf/nvim/lua/plugins/telescope.lua
@@ -32,11 +32,12 @@ return {
             pcall(require('telescope').load_extension, 'fzf')
             vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles,
                 { desc = '[?] Find recently opened files' })
-            vim.keymap.set('n', '<leader>/', require('telescope.builtin').buffers,
+            vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers,
                 { desc = '[ ] Find existing buffers' })
             vim.keymap.set('n', '<leader><leader>', "<C-^>",
                 { desc = 'Switch to previous buffer' })
 
+            vim.keymap.set('n', '<leader>/', require('telescope.builtin').resume, { desc = 'Resume' })
             vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
             vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
             vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
