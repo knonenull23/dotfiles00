773b7826d6444a3dffe7520d87e2f662e4f138bb
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-11-28 10:43:31 +0800

    Update init.lua

--- conf/init.lua
+++ conf/init.lua
@@ -190,6 +190,13 @@ require('lazy').setup({
             require('toggleterm').setup()
             vim.keymap.set("n", '<leader>`', '<CMD>ToggleTerm<CR>', { desc = "Terminal" })
             vim.keymap.set("t", '<leader>`', '<Esc><CMD>ToggleTerm<CR>', { desc = "Terminal" })
+            if vim.loop.os_uname().sysname == 'Windows' or vim.loop.os_uname().sysname == 'Windows_NT' then
+                vim.keymap.set("n", '<leader>r', '<Esc><CMD>TermExec cmd="Invoke-History"<CR>',
+                    { desc = "Run previous command in terminal" })
+            else
+                vim.keymap.set("n", '<leader>r', '<Esc><CMD>TermExec cmd="!!"<CR>',
+                    { desc = "Run previous command in terminal" })
+            end
         end
     },
     {
