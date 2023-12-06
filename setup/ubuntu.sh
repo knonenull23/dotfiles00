b7b88b7b64c41b58a12596d7361ef5811add0cc4
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-12-07 00:51:20 +0800

    update

---
+++ setup/ubuntu.sh
@@ -0,0 +1,4 @@
+sudo apt install ripgrep cmake git curl wget clangd python3-pip build-essential -y
+curl -sL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh > /tmp/miniconda.sh; sh /tmp/miniconda.sh
+curl -fsSL https://get.docker.com -o /tmp/get-docker.sh; sh /tmp/get-docker.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
