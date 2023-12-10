d00b08a4360327183997acd8761386b331944ed3
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-12-10 11:53:28 +0000

    update

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -17,3 +17,5 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 ./squashfs-root/usr/bin/nvim
+sudo cp -r ./squashfs-root/usr/* /usr/local/
+rm -r squashfs-root
