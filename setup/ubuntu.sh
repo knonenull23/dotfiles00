d7f77961ec3722d417619f9c766d233cfe3e0fb7
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-12-10 11:48:00 +0000

    update

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -12,3 +12,8 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && sudo apt update \
 && sudo apt install gh -y
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/usr/bin/nvim
