05666f63324007c25af7f742811fa86483e9087c
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-12-25 12:31:24 +0800

    update

---
+++ setup/kube.sh
@@ -0,0 +1,8 @@
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo mv minikube-linux-amd64 /usr/local/bin/minikube
+chmod +x /usr/local/bin/minikube
+
+
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo mv kubectl /usr/local/bin/kubectl
+chmod +x /usr/local/bin/kubectl
