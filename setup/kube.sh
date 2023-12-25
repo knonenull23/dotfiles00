334d6c2b35f80161d0347c0c3fbe21ded65a081f
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2023-12-25 12:36:17 +0800

    update

--- setup/kube.sh
+++ setup/kube.sh
@@ -6,3 +6,7 @@ chmod +x /usr/local/bin/minikube
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo mv kubectl /usr/local/bin/kubectl
 chmod +x /usr/local/bin/kubectl
+
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+sudo mv kustomize /usr/local/bin/kustomize
+chmod +x /usr/local/bin/kustomize
