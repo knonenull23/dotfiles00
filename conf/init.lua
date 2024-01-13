00ceab6c05d3f4fd9d1ed33397a37d49696650a0
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2024-01-13 11:43:02 +0800

    update

--- conf/init.lua
+++ conf/init.lua
@@ -405,6 +405,16 @@ require('lazy').setup({
     },
     {
         'rafamadriz/friendly-snippets',
+    },
+    {
+        'github/copilot.vim',
+        config = function()
+            vim.keymap.set('i', '<A-i>', 'copilot#Accept("\\<CR>")', {
+                expr = true,
+                replace_keycodes = false
+            })
+            vim.g.copilot_no_tab_map = true
+        end
     }
 }, {})
 
