660648fa66fccf00753beddb66a6305184876299
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2024-01-16 13:37:06 +0000

    update

--- setup/vscode-arch.sh
+++ setup/vscode-arch.sh
@@ -3,3 +3,7 @@ git clone https://aur.archlinux.org/visual-studio-code-bin.git
 cd visual-studio-code-bin
 
 makepkg -si
+
+# Run this if vscode hangs on startup
+# rm -r /home/arch/.config/Code/Cache /home/arch/.config/Code/CachedData/
+
