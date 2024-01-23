5f679d5c898ae2cd3aa16cc9d886318dc23f31bb
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2024-01-23 03:18:08 +0000

    update

--- setup/workstation-ubuntu.sh
+++ setup/workstation-ubuntu.sh
@@ -1,4 +1,4 @@
-sudo apt install ripgrep cmake git curl wget clangd python3-pip build-essential freerdp2-dev -y
+sudo apt install ripgrep cmake git curl wget clangd python3-pip build-essential freerdp2-dev dnsutils -y
 curl -sL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh > /tmp/miniconda.sh; sh /tmp/miniconda.sh
 
 curl -fsSL https://get.docker.com -o /tmp/get-docker.sh; sh /tmp/get-docker.sh
