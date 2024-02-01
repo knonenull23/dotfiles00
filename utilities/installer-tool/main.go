27c653d93add75e16eea49589486ab67d4e3285c
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2024-02-01 08:48:54 +0800

    fix errors

--- utilities/installer-tool/main.go
+++ utilities/installer-tool/main.go
@@ -2,6 +2,7 @@ package main
 
 import "github.com/rivo/tview"
 
+// main is the entry point of the program.
 func main() {
 	box := tview.NewBox().SetBorder(true).SetTitle("Installer Tool")
 	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
